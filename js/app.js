893f27fb3408662156bae4c4f04750b6c2b60c1a
Igor Sorokopud <igorztsorokopud@gmail.com>
2016-12-20 22:01:59 +0200

    add components news

--- js/app.js
+++ js/app.js
@@ -1,3 +1,19 @@
+
+var my_news = [
+    {
+        author: 'Саша Печкин',
+        text: 'В четверг, четвертого числа...'
+    },
+    {
+        author: 'Просто Вася',
+        text: 'Считаю, что $ должен стоить 35 рублей!'
+    },
+    {
+        author: 'Гость',
+        text: 'Бесплатно. Скачать. Лучший сайт - http://localhost:3000'
+    }
+];
+
 var Comments = React.createClass({
     render: function() {
         return (
@@ -10,10 +26,20 @@ var Comments = React.createClass({
 
 var News = React.createClass({
     render: function() {
+        var data = this.props.data;
+        var newsTemplate = data.map(function(item, index) {
+            return (
+                <div key={index}>
+                <p className="news__author">{item.author}:</p>
+            <p className="news__text">{item.text}</p>
+            </div>
+            )
+        })
+
         return (
             <div className="news">
-            К сожалению, новостей нет.
-        </div>
+            {newsTemplate}
+            </div>
         );
     }
 });
@@ -23,7 +49,7 @@ var App = React.createClass({
         return (
             <div className="app">
             Всем привет, я компонент App! Я умею отображать новости.
-        <News />
+        <News data={my_news} />
         <Comments />
         </div>
         );
